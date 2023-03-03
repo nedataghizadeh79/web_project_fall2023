ca9469b990e14aa615fc85da76ede62e4d7b6a6b
Negarnbkhti42 <norob35@gmail.com>
2023-03-03 13:32:17 +0330

    Add volunteership error

--- web/src/api/announcement/volunteerForAnnouncement.js
+++ web/src/api/announcement/volunteerForAnnouncement.js
@@ -3,7 +3,8 @@ import http from "../http";
 export async function volunteerForAnnouncement(volunteershipData) {
     try {
         const res = await http.post("/volunteer", volunteershipData)
+        return res.data;
     } catch (err) {
-
+        return err.errors?.[0].value()[0];
     }
 }
