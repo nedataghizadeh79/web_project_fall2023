25a2c9d84794a82c0e15c237204025ecd9949d9d
Negarnbkhti42 <norob35@gmail.com>
2023-03-03 17:08:18 +0330

    Remove state from navbar

--- web/src/components/navbar/navbar.jsx
+++ web/src/components/navbar/navbar.jsx
@@ -1,35 +1,30 @@
-import {Link} from "react-router-dom";
-import {useUser} from "../../providers/UserProvider";
+import { Link } from "react-router-dom";
 import "./navbar.css";
-import profileIcone from '../../assets/icons/use.png'
-import homePage from '../../assets/icons/home.png'
+import profileIcone from "../../assets/icons/use.png";
+import homePage from "../../assets/icons/home.png";
 
 function Navbar() {
-    const {loggedIn} = useUser();
-    return (
-        <nav className="navbar">
-            <div className="navbar__container">
-                <section className='group mainIcon'>
-                    <Link className='mainButton' to={"/main"}>صفحه اصلی</Link>
-                    <img className='Icon mainI' src={homePage} alt='main page'/>
-                </section>
+  return (
+    <nav className="navbar">
+      <div className="navbar__container">
+        <section className="group mainIcon">
+          <Link className="mainButton" to={"/main"}>
+            صفحه اصلی
+          </Link>
+          <img className="Icon mainI" src={homePage} alt="main page" />
+        </section>
 
-                <div className="navbar__left">
-                    {loggedIn ? (
-                        <section className='group'>
-                            <Link className='profileLink' to={`/profile/${12}`}>پروفایل</Link>
-                            <img className='Icon prof' src={profileIcone} alt='profile'/>
-                        </section>
-                    ) : (
-                        <>
-                            <Link to={"/login"}>ورود</Link>
-                            <Link to={"/register"}>ثبت نام</Link>
-                        </>
-                    )}
-                </div>
-            </div>
-        </nav>
-    );
+        <div className="navbar__left">
+          <section className="group">
+            <Link className="profileLink" to={`/profile/${12}`}>
+              پروفایل
+            </Link>
+            <img className="Icon prof" src={profileIcone} alt="profile" />
+          </section>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
