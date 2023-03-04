b9d94adc8bff0a32a325e9fb977d65c8468d4a00
Negarnbkhti42 <norob35@gmail.com>
2023-03-04 17:10:43 +0330

    Add transition to toast update

--- web/src/utils.js
+++ web/src/utils.js
@@ -1,11 +1,11 @@
 import { toast } from "react-toastify";
 
 export function updateToastToSuccess(id, message) {
-    toast.update(id, { id: message, type: "success", isLoading: false, autoClose: 5000, closeButton: true });
+    toast.update(id, { id: message, type: "success", isLoading: false, autoClose: 5000, closeButton: true, transition: "flip" });
 }
 
 export function updateToastToError(id, message) {
-    toast.update(id, { render: message, type: "error", isLoading: false, autoClose: 5000, closeButton: true });
+    toast.update(id, { render: message, type: "error", isLoading: false, autoClose: 5000, closeButton: true, transition: "flip" });
 
 }
 
